e8fef9bc3ea846852d8dac8b243d2db9a7ea3132
DarkPixel100 <diegofavila20@gmail.com>
2022-10-31 12:00:57 -0300

    Setup Inicial -> header montado

---
+++ php/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home - Clínica Hsucesso</title>
+    <link rel="stylesheet" href="../node_modules/normalize.css/normalize.css">
+    <link rel="stylesheet" href="../node_modules/milligram/dist/milligram.css">
+    <link rel="stylesheet" href="../css/geral.css">
+</head>
+
+<body>
+    <div id="header">
+        <img src="../img/Clinica Hsucesso-Logo.svg" alt="Logo_Hsucesso">
+        <div id="abas">
+            <a id="login" href="./login.php">LOGIN</a>
+            <a id="contatos" href="./contatos.php">CONTATOS</a>
+        </div>
+        <nav>
+            <a href="./exames.php">EXAMES</a>
+            <a href="./resultados.php">RESULTADOS</a>
+            <a href="./convenios.php">CONVÊNIOS</a>
+        </nav>
+    </div>
+    <div id="wrapper">
+        <button>Hello</button>
+        <?php
+        //só usa pra forms
+        ?>
+    </div>
+</body>
+
+</html>
