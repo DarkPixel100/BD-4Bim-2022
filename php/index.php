7e110fcdb809ffcb356c6ee9d5826dae3160e88d
DarkPixel100 <diegofavila20@gmail.com>
2022-11-21 12:03:13 -0300

    imagens+logoNova+grid+proporções
    Co-autoredby: Sofia Machado <sofia25.machado@gmail.com>

--- php/index.php
+++ php/index.php
@@ -12,24 +12,49 @@
 </head>
 
 <body>
-    <?php include './header_inc.php';?>
+    <?php include './header_inc.php'; ?>
     <div id="wrapper">
         <div id="inicio">
             <h1>Atendimento</h1>
             <p>HORÁRIO DE COLETAS:<br>Unidade Matriz - Rio Grande: 07hs às 17:30h - sáb. 7hs às 11:30hs<br>Unidade Filial - São José do Norte: Seg a Sex das 07:00 as 10:00 (não abre aos sábados)</p>
         </div>
         <div id="caixas">
-            <div id="Resultados-box">
-            Resultados<br>Você será informado via SMS, quando os resultados dos seus exames já estiverem disponíveis em nosso site.
+            <div>
+                <h2>Resultados</h2>
+                <p>
+                    Você será informado via SMS, quando os resultados dos seus exames já estiverem disponíveis em nosso site.
+                </p>
             </div>
-            <div id="Agendamento-box">
-            Agendamento<br>Agende sua consulta de maneira on-line
+            <div>
+                <h2> Agendamento </h2>
+                <p> Agende sua consulta de maneira on-line</p>
             </div>
-            <div id="Exame-box">
-            Exames<br>Escolha uma das opções de exames disponíveis
-            </div>        
+            <div>
+                <h2> Exames </h2>
+                <p> Escolha uma das opções de exames disponíveis </p>
+            </div>
         </div>
-        <h3>Tabela importada do banco com PHP:</h3>
+        <h2>Sempre perto de você</h2>
+        <p>Estamos evoluindo sempre, focados no bem estar dos nossos clientes, buscando proporcionar o carinho e conforto a que todos merecem</p>
+        <div id="fotosLocal">
+            <div>
+                <img src="../img/matrizRG.png" alt="">
+                <h3>Matriz Rio Grande</h3>
+                <p>A Matriz Rio Grande disponibiliza um amplo local para facilitar ainda mais o atendimento, coleta e retirada de resultados.</p>
+            </div>
+            <div>
+                <img src="../img/recepcao.png" alt="">
+                <h3>Recepção</h3>
+                <p>Recepção com todo o conforto para melhor atender.</p>
+            </div>
+            <div>
+                <img src="../img/espacoKids.png" alt="">
+                <h3>Espaço Kids</h3>
+                <p>Espaço personalizado para agradar e entreter as crianças.</p>
+            </div>
+        </div>
+        
+        <!-- <h3>Tabela importada do banco com PHP:</h3>
         <?php
         $db = new SQLite3("../db/hsucesso.db");
         $db->exec("PRAGMA foreign_keys = ON");
@@ -53,7 +78,7 @@
         }
         echo '</table>';
         $db->close();
-        ?>
+        ?> -->
     </div>
 </body>
 
